0df5547b97299f66df008201009fde9611575a3b
Victor Luã <56847759+victorluadev@users.noreply.github.com>
2024-11-12 01:47:29 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
 
 function Home() {
-    const [isClient, setIsClient] = useState(false)
+  const [isClient, setIsClient] = useState(false);
 
-    useEffect(() => {
-        setIsClient(true)
-    }, [])
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-    function getRandomIntInclusive(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-    return <h1>{isClient ? `Já fazem ${getRandomIntInclusive(1000, 9999)} dias que reclamam do Javascript no Backend 😆` : ""}</h1>
+  return (
+    <h1>
+      {isClient
+        ? `Já fazem ${getRandomIntInclusive(1000, 9999)} dias que reclamam do Javascript no Backend 😆`
+        : ""}
+    </h1>
+  );
 }
 
 export default Home;
